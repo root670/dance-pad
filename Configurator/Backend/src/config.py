3a764f09dd44187b82e4dced5e4d7766c02f9ddd
Wes Castro <root670@gmail.com>
2020-11-26 18:56:17 -0800

    Add config loader.

--- Configurator/Backend/src/config.py
+++ Configurator/Backend/src/config.py
@@ -1,3 +1,11 @@
-#
-# Store for configuration settings
-#
+"""Configurable settings.
+"""
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+CONFIG = dict(
+    SERIAL_DEVICE=os.getenv('SERIAL_DEVICE')
+)
