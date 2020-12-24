202a93f9b7205f9042983c1af96fd3f8892f86a6
Wes Castro <root670@gmail.com>
2020-12-24 13:55:30 -0800

    Implement configuration class.

--- Firmware/include/Config.h
+++ Firmware/include/Config.h
@@ -3,31 +3,62 @@
 //
 #pragma once
 #include <Arduino.h>
+#include <EEPROM.h>
+#include <map>
 
 class Configuration
 {
 public:
-    Configuration() {};
+    Configuration()
+        : m_bLoaded(false), m_bDirty(false)
+    {};
 
-    typedef enum configVersion
+    // Strings
+    const String& getString(const String &strKey, const String &strDefault)const;
+    void setString(const String &strKey, const String &strValue);
+
+    // 32-bit unsigned integers
+    const uint32_t& getUInt32(const String &strKey, const uint32_t &nDefault)const;
+    void setUInt32(const String &strKey, uint32_t nValue);
+
+    // Read configuration from EEPROM
+    void read();
+
+    // Write configuration to EEPROM if data is dirty
+    void write();
+
+private:
+
+    // Write a null-terminated string to the EEPROM at nOffset.
+    // Returns offset immediately after the null-terminator.
+    int put(int nOffset, const String &str)const;
+
+    // Write a POD value to the EEPROM at nOffset.
+    // Returns the offset immediately after the value.
+    template<typename TYPE>
+    int put(int nOffset, TYPE nValue)const
     {
-        enumVersion1 = 1,
-        enumVersionCurrent = enumVersion1
-    } configVersion_t;
+        EEPROM.put(nOffset, nValue);
+        return nOffset + sizeof(nValue);
+    }
 
-    /*
-    Representation in EEPROM:
-    setinel value
-    version
-    crc of values 1 through n
-    value 1
-    value 2
-    ...
-    value n
-    */
+    // Get a null-terminated string from the EEPROM at nOffset.
+    // Returns offset immediately after the null-terminator.
+    int get(int nOffset, String &str)const;
 
-    bool readFromEEPROM();
-    bool writeToEEPROM();
+    // Get a POD value from the EEPROM at nOffset.
+    // Returns offset immediately after the value.
+    template<typename TYPE>
+    int get(int nOffset, TYPE &nValue)const
+    {
+        EEPROM.get(nOffset, nValue);
+        Serial.printf("get: %08X\n", nValue);
+        return nOffset + sizeof(nValue);
+    }
+
+    bool                        m_bLoaded, m_bDirty;
+    std::map<String, String>    m_mapStr;
+    std::map<String, uint32_t>  m_mapUInt32;
 };
 
-Configuration g_config;
+extern Configuration g_config;
--- Firmware/src/Config.cpp
+++ Firmware/src/Config.cpp
@@ -1,11 +1,121 @@
 #include "Config.h"
+#include <EEPROM.h>
 
-bool Configuration::writeToNVRAM()
+Configuration g_config;
+static const uint32_t kSentinelValue = 0x5AFEC0DE;
+
+const String& Configuration::getString(const String &strKey, const String &strDefault)const
 {
-
+    auto it = m_mapStr.find(strKey);
+    if(it == m_mapStr.end())
+        return strDefault;
+    
+    return it->second;
 }
 
-bool Configuration::readFromNVRAM()
+void Configuration::setString(const String &strKey, const String &strValue)
 {
+    m_mapStr[strKey] = strValue;
+    m_bDirty = true;
+}
+
+const uint32_t& Configuration::getUInt32(const String &strKey, const uint32_t &nDefault)const
+{
+    auto it = m_mapUInt32.find(strKey);
+    if(it == m_mapUInt32.end())
+        return nDefault;
+
+    return it->second;
+}
+
+void Configuration::setUInt32(const String &strKey, uint32_t nValue)
+{
+    m_mapUInt32[strKey] = nValue;
+    m_bDirty = true;
+}
+
+int Configuration::put(int nOffset, const String &str)const
+{
+    const char *pData = str.c_str();
+    while(*pData)
+    {
+        EEPROM.put(nOffset++, *pData++);
+    }
+    EEPROM.put(nOffset++, '\0');
+    return nOffset;
+}
+
+int Configuration::get(int nOffset, String &str)const
+{
+    char c;
+    while(EEPROM.get(nOffset++, c))
+    {
+        str += c;
+    }
+
+    Serial.printf("get[str]: %s\n", str.c_str());
     
+    return nOffset;
+}
+
+void Configuration::read()
+{
+    // Get sentinel value
+    int nOffset(0);
+    uint32_t nSentinel;
+    nOffset = get(nOffset, nSentinel);
+
+    if(nSentinel != kSentinelValue)
+        return; // Uninitialized or bad data
+
+    // Get strings
+    int nNumStrings;
+    nOffset = get(nOffset, nNumStrings);
+    for(int nString = 0; nString < nNumStrings; nString++)
+    {
+        String strKey, strValue;
+        nOffset = get(nOffset, strKey);
+        nOffset = get(nOffset, strValue);
+        m_mapStr[strKey] = strValue;
+    }
+
+    // Get 32-bit unsigned integers
+    int nNumUInt32;
+    nOffset = get(nOffset, nNumUInt32);
+    for(int nElement = 0; nElement < nNumUInt32; nElement++)
+    {
+        String strKey;
+        uint32_t nValue;
+        nOffset = get(nOffset, strKey);
+        nOffset = get(nOffset, nValue);
+        m_mapUInt32[strKey] = nValue;
+    }
+
+    m_bDirty = false;
+}
+
+void Configuration::write()
+{
+    if(m_bDirty)
+    {
+        // Write sentinel value
+        int nOffset(0);
+        nOffset = put(nOffset, kSentinelValue);
+
+        // Write strings
+        nOffset = put(nOffset, m_mapStr.size());
+        for(auto const &element : m_mapStr)
+        {
+            nOffset = put(nOffset, element.first);
+            nOffset = put(nOffset, element.second);
+        }
+
+        // Write 32-bit unsigned integers
+        nOffset = put(nOffset, m_mapUInt32.size());
+        for(auto const &element : m_mapUInt32)
+        {
+            nOffset = put(nOffset, element.first);
+            nOffset = put(nOffset, element.second);
+        }
+    }
 }
