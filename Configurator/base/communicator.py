6e51bab9f347ae7c1e324a69e0b8dd6c3576f13f
Wes Castro <root670@gmail.com>
2020-12-25 14:56:52 -0800

    Read new value command response.
    Add `set_thresholds` function.

--- Configurator/base/communicator.py
+++ Configurator/base/communicator.py
@@ -37,8 +37,17 @@ class Communicator:
 
     COMMAND_VERSION = 'version'
     COMMAND_BLINK = 'blink'
-    COMMAND_PANELCONFIG = 'panelconfig'
+    COMMAND_GETCONFIG = 'config'
+    COMMAND_SETCONFIG = 'set'
+    COMMAND_PERSIST = 'persist'
     COMMAND_VALUES = 'v'
+
+    RESPONSE_SUCCESS = '!'
+    RESPONSE_FAILURE = '?'
+    RESPONSES = set({
+        Communicator.RESPONSE_SUCCESS,
+        Communicator.RESPONSE_FAILURE
+    })
 
     def __init__(
         self,
@@ -51,18 +60,46 @@ class Communicator:
         """
         self._ser = ser
 
+    def __send_line(self, line: str) -> None:
+        """Send line terminated with newline character.
+
+        Args:
+            line: The line to send.
+        """
+        self._ser.write(f'{line}\n'.encode('ascii'))
+
+
+    def __get_line(self) -> str:
+        """Get ASCII-encoded line from the device.
+        """
+        return self._ser.readline().decode('ascii').strip()
+
     def __send_command(self, command: str) -> None:
         """Send command to the device.
 
         Args:
             command: Command string.
         """
-        self._ser.write(f'-{command}\n'.encode('ascii'))
+        self.__send_line(f'-{command}')
 
-    def __get_line(self) -> str:
-        """Get ASCII-encoded line from the device.
+    def __set_u16_config(self, key: str, value) -> bool:
+        """Set a configuration item for a 16-bit unsigned value.
+
+        Args:
+            key: Name of configuration item.
+            value: The value to set the configuration item to.
+
+        Returns:
+            True if the value was set successfully. Otherwise, False.
         """
-        return self._ser.readline().decode('ascii').strip()
+        self.__send_command(self.COMMAND_SETCONFIG)
+        self.__send_line(f'u16 {key}={value}')
+        response = self.__get_line()
+
+        if response == self.RESPONSES:
+            raise ValueError(f'Unexpected response: {response}')
+
+        return response == self.RESPONSE_SUCCESS
 
     def get_version(self) -> str:
         """Get firmware version string.
@@ -75,53 +112,168 @@ class Communicator:
         """
         self.__send_command(self.COMMAND_BLINK)
 
-    def get_panel_config(self) -> Mapping[str, Enum]:
-        """Get configuration of panels.
+    def set_thresholds(self, pin: int, trigger: int, release:int) -> bool:
+        """Set the tresholds for a sensor.
+
+        Args:
+            pin: Pin on the Teensy connected to the sensor.
+            trigger: Value above baseline to trigger a hit.
+            release: Value above baseline to trigger a release.
 
         Returns:
-            Dictionary of configurations for each panel.
+            True if the threshold was set successfully. Otherwise, False.
         """
-        self.__send_command(self.COMMAND_PANELCONFIG)
+        if not 0 < trigger < 1024:
+            raise ValueError('`trigger` must be in range [0, 1024).')
+        if not 0 < release < 1024:
+            raise ValueError('`release` must be in range [0, 1024).')
+        if release > trigger:
+            raise ValueError(
+                '`trigger` must be greater than or equal to `release`'
+            )
+
+        key_name = f'sensor{pin}'
+
+        return self.__set_u16_config(key_name + 'trigger', trigger) and \
+            self.__set_u16_config(key_name + 'release', release)
+
+    def get_config(self) -> Mapping[str, Enum]:
+        """Get configuration.
+
+        Returns:
+            Dictionary of configuration items.
+        """
+        self.__send_command(self.COMMAND_GETCONFIG)
         response = self.__get_line()
         split = response.split(',')
         return dict(
-            up=PanelOrientation.from_degrees(int(split[0])),
-            down=PanelOrientation.from_degrees(int(split[1])),
-            left=PanelOrientation.from_degrees(int(split[2])),
-            right=PanelOrientation.from_degrees(int(split[3])),
+            up=dict(
+                orientation=PanelOrientation.from_degrees(int(split.pop(0))),
+                north_pin=int(split.pop(0)),
+                east_pin=int(split.pop(0)),
+                south_pin=int(split.pop(0)),
+                west_pin=int(split.pop(0)),
+            ),
+            down=dict(
+                orientation=PanelOrientation.from_degrees(int(split.pop(0))),
+                north_pin=int(split.pop(0)),
+                east_pin=int(split.pop(0)),
+                south_pin=int(split.pop(0)),
+                west_pin=int(split.pop(0)),
+            ),
+            left=dict(
+                orientation=PanelOrientation.from_degrees(int(split.pop(0))),
+                north_pin=int(split.pop(0)),
+                east_pin=int(split.pop(0)),
+                south_pin=int(split.pop(0)),
+                west_pin=int(split.pop(0)),
+            ),
+            right=dict(
+                orientation=PanelOrientation.from_degrees(int(split.pop(0))),
+                north_pin=int(split.pop(0)),
+                east_pin=int(split.pop(0)),
+                south_pin=int(split.pop(0)),
+                west_pin=int(split.pop(0)),
+            ),
         )
 
     def get_sensor_values(self) -> Mapping[str, Mapping[str, int]]:
         """Get current raw sensor values.
 
         Returns:
-            Dictionary with mapping an arrow direction to dictionary of values for each sensor.
+            Nested dictionary mapping arrow directions to cardinal
+            direction sensors with `value`, `trigger_threshold`, and
+            `release_threshold` values.
         """
         self.__send_command(self.COMMAND_VALUES)
         values = self.__get_line().split(',')
         return dict(
             up=dict(
-                north=int(values.pop(0)),
-                east=int(values.pop(0)),
-                south=int(values.pop(0)),
-                west=int(values.pop(0)),
+                north=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                east=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                south=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                west=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
             ),
             down=dict(
-                north=int(values.pop(0)),
-                east=int(values.pop(0)),
-                south=int(values.pop(0)),
-                west=int(values.pop(0)),
+                north=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                east=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                south=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                west=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
             ),
             left=dict(
-                north=int(values.pop(0)),
-                east=int(values.pop(0)),
-                south=int(values.pop(0)),
-                west=int(values.pop(0)),
+                north=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                east=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                south=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                west=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
             ),
             right=dict(
-                north=int(values.pop(0)),
-                east=int(values.pop(0)),
-                south=int(values.pop(0)),
-                west=int(values.pop(0)),
+                north=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                east=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                south=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
+                west=dict(
+                    value=int(values.pop(0)),
+                    trigger_threshold=int(values.pop(0)),
+                    release_threshold=int(values.pop(0)),
+                ),
             ),
         )
