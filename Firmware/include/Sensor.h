67393f1f9278a2769206a6b8d451ee0a2787a3f1
Wes Castro <root670@gmail.com>
2020-12-24 16:25:51 -0800

    Get trigger/release thresholds from global config.

--- Firmware/include/Sensor.h
+++ Firmware/include/Sensor.h
@@ -2,15 +2,19 @@
 
 #include <Arduino.h>
 
+#include "Config.h"
+
 class Sensor
 {
 public:
     Sensor(uint8_t nPin)
     {
+        String strIdentifier("sensor" + nPin);
+
         m_nPin              = nPin;
         m_nPressure         = 0;
-        m_nTriggerOffset    = 50;
-        m_nReleaseOffset    = 25;
+        m_nTriggerOffset    = g_config.getUInt16(strIdentifier + "trigger", 50);
+        m_nReleaseOffset    = g_config.getUInt16(strIdentifier + "release", 25);
         m_nTriggerThreshold = 0;
         m_nReleaseThreshold = 0;
         m_nLastChangeTimeMS = 0;
